7573f24ee9a5f41c4ef892f3fdc67f3d24432a28
jmrigali <jmrigali@ymail.com>
2017-06-05 17:26:23 -0600

    complete part 1 and part 2

--- routes/books.js
+++ routes/books.js
@@ -4,7 +4,78 @@ const express = require('express');
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
+const knex = require('../knex');
+const humps = require('humps');
 
-// YOUR CODE HERE
+router.get('/books', (req, res, next)=> {
+  knex('books')
+  .orderBy('title')
+  .then((result) => {
+    result = humps.camelizeKeys(result);
+    return res.send(result);
+  }).catch((err) => {
+    console.error(err);
+  });
+});
 
+router.get('/books/:id', (req, res, next)=> {
+  knex('books')
+  .where('id', req.params.id)
+  .then((result)=> {
+    result = humps.camelizeKeys(result);
+    res.send(result[0]);
+  }).catch((err) => {
+    console.error(err);
+  });
+});
+
+router.post('/books', (req, res, next)=> {
+  knex('books')
+  .insert({
+    title: req.body.title,
+    author: req.body.author,
+    genre: req.body.genre,
+    description: req.body.description,
+    cover_url:req.body.coverUrl
+  }, '*')
+  .then((result)=>{
+    result = humps.camelizeKeys(result);
+    res.send(result[0]);
+  })
+  .catch((err)=> {
+    console.error(err);
+  });
+});
+
+router.patch('/books/:id', (req, res, next)=>{
+  knex('books')
+  .where('id', req.params.id)
+  .update({
+    title: req.body.title,
+    author: req.body.author,
+    genre: req.body.genre,
+    description: req.body.description,
+    cover_url:req.body.coverUrl
+  }, '*')
+  .then((result)=> {
+    result = humps.camelizeKeys(result);
+    res.send(result[0]);
+  }).catch((err)=> {
+    console.error(err);
+  })
+});
+
+router.delete('/books/:id', (req, res, next)=> {
+  knex('books')
+  .where('id', req.params.id)
+  .returning('*')
+  .del()
+  .then((result)=> {
+    result = humps.camelizeKeys(result);
+    delete result[0].id;
+    res.send(result[0]);
+  }).catch((err)=> {
+    console.error(err);
+  })
+})
 module.exports = router;
